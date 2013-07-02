d3b5dfa991a1106ee7a85234c9ac9879af58e53b
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2013-07-02 22:45:35 +0200

    Testing git

--- my-ttt.hs
+++ my-ttt.hs
@@ -5,4 +5,4 @@ board = "12345678"
 
 showBoard :: Board -> String
 showBoard b =
-  b !! 0 : [] ++ " | " ++ b !! 1 : [] ++ " | " ++ b !! 2 : []
+  b !! 0 : [] ++ " | " ++ b !! 1 : [] ++ " | " ++ b !! 2 : [] \n
